de1c807da5ec6f2a7c34bf36b1b92fc8dffaf628
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-09-12 15:53:21 +0100

    feat: create TrackList component

---
+++ src/components/TrackList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
+
+const TrackList = () => {
+  const trackList = [{ name: 'track1' }, { name: 'track2' }, { name: 'track3' }];
+
+  const playTrack = (index) => {
+    console.log('playTrack', index);
+  }
+
+  const isPlayed = (name) => {
+    console.log('isPlayed', name)
+    return true;
+  }
+
+  return (
+    <>
+      {trackList.map((track, index) => (
+        <div className="box" key={index}>
+          <button className="button" onClick={() => playTrack(index)}>
+            {isPlayed() ? <FontAwesomeIcon icon={faPlay} /> : <FontAwesomeIcon icon={faPause} />}
+          </button>
+          <div className="song-title">
+            {track.name}
+          </div>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default TrackList;
