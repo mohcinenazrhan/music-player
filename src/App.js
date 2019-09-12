40d5f39d71e22c0f3a7790e2eda0f793ad462170
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-09-12 16:01:33 +0100

    feat: call the playerControls and TrackList components to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import TrackList from './components/TrackList';
+import PlayerControls from './components/PlayerControls';
 
 function App() {
-	return <div className="container" />;
+	return (
+		<div className="container">
+			<TrackList />
+			<PlayerControls />
+		</div>
+	);
 }
 
 export default App;
