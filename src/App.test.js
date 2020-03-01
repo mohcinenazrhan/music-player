f288e7e0e4524ee856c5de4536a3818f850549f0
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2020-03-01 22:57:30 +0100

    test: renders without crashing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom'
+
 import App from './App';
 
+afterEach(cleanup);
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+	ReactDOM.unmountComponentAtNode(div);
 });
