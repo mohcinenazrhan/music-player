96c86695bc5d3154434b724d54e05411659082b7
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2020-03-01 22:59:17 +0100

    test: should all the controls btns be disabled

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,7 @@ import '@testing-library/jest-dom'
 
 import App from './App';
 import TrackList from './components/TrackList';
+import PlayerControls from './components/PlayerControls';
 import { MusicPlayerProvider } from './MusicPlayerContext';
 import tracks from './tracks';
 
@@ -27,5 +28,21 @@ it('should display all the tracks name', () => {
 
 	for (const track of tracks) {
 		getByText(track.name);
+	}
+});
+
+it('should all the controls btns be disabled', () => {
+	const { getByLabelText } = render(
+		<MusicPlayerProvider>
+			<div className="container">
+				<PlayerControls />
+			</div>
+		</MusicPlayerProvider>
+	);
+
+	const contolsBtnsAriaLabel = ['Play the previous track', 'Play the track', 'Play the next track'];
+
+	for (const label of contolsBtnsAriaLabel) {
+		expect(getByLabelText(label)).toHaveAttribute('disabled');
 	}
 });
