61648f0cfc9d9383a3c026152ba4b3dcc34e4c6f
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2020-03-01 22:58:20 +0100

    test: should display all the tracks name

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { cleanup } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom'
 
 import App from './App';
+import TrackList from './components/TrackList';
+import { MusicPlayerProvider } from './MusicPlayerContext';
+import tracks from './tracks';
 
 afterEach(cleanup);
 
@@ -11,4 +14,18 @@ it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<App />, div);
 	ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should display all the tracks name', () => {
+	const { getByText } = render(
+		<MusicPlayerProvider>
+			<div className="container">
+				<TrackList />
+			</div>
+		</MusicPlayerProvider>
+	);
+
+	for (const track of tracks) {
+		getByText(track.name);
+	}
 });
