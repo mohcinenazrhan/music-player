002832cca487ed268f88c938584872cbc2af1e4c
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2020-03-01 22:52:19 +0100

    refactor: move tracks data to separate file to make it reusable

---
+++ src/tracks.js
@@ -0,0 +1,18 @@
+import happyrock from './audios/happyrock.mp3';
+import jazzyfrenchy from './audios/jazzyfrenchy.mp3';
+import summer from './audios/summer.mp3';
+
+export default [
+  {
+    name: 'Happy Rock - Genesis',
+    file: happyrock
+  },
+  {
+    name: 'Jazzy Frenchy - Shaken Soda',
+    file: jazzyfrenchy
+  },
+  {
+    name: 'Summer - Such Fun',
+    file: summer
+  }
+];
